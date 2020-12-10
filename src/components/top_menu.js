06076e8d0c12dc5528a52c616904514c9f2ded52
Demitra Del Fiacco <ddelfiacco99@gmail.com>
2020-12-10 02:38:12 -0600

    DONE

--- src/components/top_menu.js
+++ src/components/top_menu.js
@@ -6,6 +6,7 @@ export default function TopBar() {
       <Menu.Item><Icon name='book' /></Menu.Item>
       <Menu.Item><p style={{ color: "blueviolet"}}>Computing, Ethics, & Society Course Learning Module</p></Menu.Item>
       <Menu.Item><p style={{ color: "grey"}}>By Anthony Roytman and Demitra Del Fiacco</p></Menu.Item>
+      <Menu.Item><a href="https://docs.google.com/document/d/1LE8tOW8WgnFBi7342KMjztLx1lqkRQVq2EZrxBmM0bo/edit?usp=sharing" target="_blank">Link To Sources</a></Menu.Item>
     </Menu>
   )
 }
